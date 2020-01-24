bdcca8abe9db0af6927df135bac9df78b14f3aa0
ak-fishbarc <60137170+ak-fishbarc@users.noreply.github.com>
2020-01-24 23:08:02 +0000

    Add files via upload

--- Learning/LearningFlask.py
+++ Learning/LearningFlask.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, render_template, flash, url_for, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 
@@ -33,6 +35,35 @@ class articles(db.Model):
         self.article = article
         self.timestamp = timestamp
 
+
+class archives(db.Model):
+    __tablename__ = 'archives'
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(20))
+    article = db.Column(db.String(1000))
+    timestamp = db.Column(db.String(40))
+
+    def __init__(self, author, article, timestamp):
+        self.author = author
+        self.article = article
+        self.timestamp = timestamp
+
+# Data visuals
+
+class departments_data(db.Model):
+    __tablename__ = 'departments'
+    name = db.Column(db.String(50), primary_key=True)
+    workforce = db.Column(db.Integer)
+    lateness = db.Column(db.Integer)
+    sickness = db.Column(db.Integer)
+    accidents = db.Column(db.Integer)
+
+    def __init__(self, name, workforce, lateness, sickness, accidents):
+        self.name = name
+        self.workforce = workforce
+        self.lateness = lateness
+        self.sickness = sickness
+        self.accidents = accidents
 
 @app.route('/')
 def index():
@@ -76,6 +107,51 @@ def verify():
     else:
         return 'Oops, something went wrong'
 
+@app.route('/departmens', methods=['POST', 'GET'])
+def departments():
+    if session['username']:
+        db_object = workers.query.get(session['username'])
+        if 'Admin' in db_object.Position:
+            departments_object = departments_data.query.all()
+            departments_list = []
+            for department in departments_object:
+                department_dict = {}
+                department_dict['name'] = department.name
+                department_dict['workforce'] = department.workforce
+                department_dict['lateness'] = department.lateness
+                department_dict['sickness'] = department.sickness
+                department_dict['accidents'] = department.accidents
+                departments_list.append((department_dict))
+            departments_prepared = json.dumps(departments_list)
+            return render_template('departments.html', name=session['username'], departments_object=departments_object
+                                   , departments_prepared=departments_prepared)
+        else:
+            flash('No permission. Please log-in.')
+            return redirect(url_for('index.html'))
+    else:
+        flash('Something went wrong. Please log-in.')
+        return redirect(url_for('index.html'))
+
+@app.route('/add_department', methods=['POST', 'GET'])
+def add_departments():
+    if request.method == 'POST':
+        department_name = request.form["name"]
+        department_workforce = request.form["workforce"]
+        department_lateness = request.form["lateness"]
+        department_sickness = request.form["sickness"]
+        department_accidents = request.form["accidents"]
+        new_department = departments_data(name=department_name, workforce=department_workforce, lateness=department_lateness, sickness=department_sickness, accidents=department_accidents)
+        try:
+            db.session.add(new_department)
+            db.session.commit()
+            departments_object = departments_data.query.all()
+            return redirect(url_for('departments', name=session['username'], departments_object=departments_object))
+        except Exception as e:
+            print(e)
+            return "Oops, something went wrong"
+    else:
+        return "Oops, something went wrong"
+
 
 @app.route('/add_article', methods=['POST', 'GET'])
 def add_article():
@@ -93,6 +169,9 @@ def add_article():
 @app.route('/delete_article/<id>', methods=['DELETE', 'POST', 'GET'])
 def delete_article(id):
     article = articles.query.get(id)
+    new_archive = archives(author=article.author, article=article.article, timestamp=article.timestamp)
+    db.session.add(new_archive)
+    db.session.commit()
     db.session.delete(article)
     db.session.commit()
     return redirect(url_for('verify', name=session['username']))
